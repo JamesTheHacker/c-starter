cde0c21f25d4ea029a678d838d01d61c1c372115
James Jeffery <jame.slovescode@gmail.com>
2021-07-21 21:52:59 +0100

    Added test suite

--- tests/test_hello.c
+++ tests/test_hello.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <check.h>
 #include "../include/hello.h"
 
@@ -8,7 +9,35 @@ START_TEST (test_hello)
 }
 END_TEST
 
+Suite* hello_suite()
+{
+    Suite *suite;
+    TCase *tc_core;
+
+    suite = suite_create("Hello");
+
+    /* Core test cases */
+    tc_core = tcase_create("Core");
+
+    /* Add test cases */
+    tcase_add_test(tc_core, test_hello);
+    suite_add_tcase(suite, tc_core);
+
+    return suite;
+}
+
 int main(int argc, char **argv)
 {
-    return 0;
+    int number_failed;
+    Suite *suite;
+    SRunner *suite_runner;
+
+    suite = hello_suite(); 
+    suite_runner = srunner_create(suite);
+
+    srunner_run_all(suite_runner, CK_NORMAL);
+    number_failed = srunner_ntests_failed(suite_runner);
+    srunner_free(suite_runner);
+
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
